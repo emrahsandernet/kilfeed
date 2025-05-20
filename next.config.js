06ae8e5928c644ebd92c90b3a5a705c39a86246f
Emrah Şander <emrahsander@Emrah-MacBook-Air.local>
2025-05-20 20:45:18 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['localhost', '192.168.1.21'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '192.168.1.21',
+        port: '3000',
+        pathname: '/maps/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '192.168.1.21',
+        port: '3000',
+        pathname: '/logos/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/maps/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/logos/**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig; 
